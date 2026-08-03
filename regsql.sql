5dd9006911d3475dc3b19ecab7926a1b6ef5bce1
Ritayan Raychaudhuri <ritayanrc2008@gmail.com>
2026-08-03 21:06:03 +0530

    Delete regsql.sql

--- regsql.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE registration_db;
-USE registration_db;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    regno VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
